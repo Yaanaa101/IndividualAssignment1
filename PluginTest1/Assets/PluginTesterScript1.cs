5aaef2ab0bb80d496fd5c27765134179392bb0e6
Katie Tong <katie.tong@ontariotechu.net>
2019-09-27 13:05:15 -0400

    Upload Project
    Upload entire project because forgot to create repo

---
+++ PluginTest1/Assets/PluginTesterScript1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class PluginTesterScript1 : MonoBehaviour
+{
+    const string DLL_NAME = "SIMPLEPLUGIN";
+    [DllImport(DLL_NAME)]
+    private static extern int SimpleFunction();
+
+    [DllImport(DLL_NAME)]
+    private static extern void read();
+
+    [DllImport(DLL_NAME)]
+    private static extern void write(float numX, float numY, float numZ);
+
+    [DllImport(DLL_NAME)]
+    private static extern float getX();
+
+    [DllImport(DLL_NAME)]
+    private static extern float getY();
+
+    [DllImport(DLL_NAME)]
+    private static extern float getZ();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {   
+            Debug.Log(SimpleFunction());
+        }
+
+        //saves the position of the cube to the txt file
+        if (Input.GetKey(KeyCode.O))
+        {
+            //writes the position of the x,y,z of the cube to the txt file
+            write(transform.position.x, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            read();
+
+            //getting the transformed position and setting the cube's position to that
+            transform.position = new Vector3(getX(), getY(), getZ());
+         
+        }
+    }
+}
---
+++ PluginTest1/Assets/cubeScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * 10.0f * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * 10.0f * Time.deltaTime;
+        }
+        //didnt use s because when s is pressed it begins to debug instead
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * 10.0f * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * 10.0f * Time.deltaTime;
+        }
+
+
+    }
+}
